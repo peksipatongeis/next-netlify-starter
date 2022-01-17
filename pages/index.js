fd35da98a7ccdeaf9cc365bbb905b8f863535f66
Pekka Wallenius <pekka.wallenius@zure.com>
2022-01-17 17:10:57 +0200

    Testing next/image

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -13,11 +14,21 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
+          <div
+            style={{
+              width: '250px',
+              height: '250px',
+              margin: '0 auto',
+              position: 'relative',
+            }}
+          >
+            <Image src={'/maito.jpg'} layout="fill" />
+          </div>
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
